76f7fcefd426a002126806416e8cecc464cbc255
Zhenyuan Gao <kegao@westmont.edu>
2019-11-24 18:57:29 -0800

    kevin get the two player mode to work.

--- connect4.rkt
+++ connect4.rkt
@@ -8,7 +8,7 @@
 
 (define rows 6)
 (define columns 7)
-(define game-state (new state% [current-board (reset-board)] [current-turn 1] [current-status 1]))
+(define game-state (new state% [current-board (reset-board 6 7)] [current-turn 1] [current-status 1]))
 
 ;(drop-piece game-state 0 5 columns)
 ;(for ([i '(0 1 2)])
@@ -34,52 +34,50 @@
                (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
                            
 ;while winning conditions are not satisfied, we keep doing the game loop
+;check the status of the game
 (define (game-loop)
-  (if (check-win game-state 0 columns)
+  (if (= (send game-state get-status) 0)
       0
       (let ()
         (while  (= (drop-piece game-state (scanner) 5 columns) -2)(continue)  )
-                        
+         (check-win game-state 0 columns)
 
         ;(send game-state get-board))
         ;change turn after each player played and get the newest game state.
+        ;checkwin 0 keep running game status is 0 stop running. When checkwin has three different input 1 means player1 wins,2 means player 2 wins,3 means it is a tie. for winning.
 
-        (check-win game-state 0 columns)
-        (display (check-win game-state 0 columns))
-        (send game-state change-turn)
-        ;check the win condition again
-  
-        ;print out the board again.
-        (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
-        (game-loop)))) 
+       (cond [(= (check-win game-state 0 columns) 1)(let ()(send game-state update-status 0)(display "player1 win"))]
+             [(= (check-win game-state 0 columns) 2)(let ()(send game-state update-status 0)(display "player2 win"))]
+             [(= (check-win game-state 0 columns) 3)(let ()(send game-state update-status 0)(display "it is a draw"))]
+            [(= (check-win game-state 0 columns) 0)(let ()
+             (display (check-win game-state 0 columns))
+             (send game-state update-status 1)
+             
+             )]
+
+
+              )
+             
+             
+             
+            (send game-state change-turn)
+           (print (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+                      (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+
+           )
+        
+        (game-loop))
+
+
+      )
+  ) 
  
  
   ;(display "you win lol")
   
 
 
-;(define (game-loop) (cond [(not (check-win game-state 0 columns))(let () ()     )]))
 
-;show the board object
-;(send f show #t)
-;
-;(drop-piece game-state (scanner) 5 columns)
-;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
-;(drop-piece game-state (scanner) 5 columns)
-;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
-;(drop-piece game-state (scanner) 5 columns)
-;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
-;(drop-piece game-state (scanner) 5 columns)
-;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
-
-
-;(check-win game-state 0 columns)
-;call game loop
  (game-loop )
 ;()
 ;(while  (= (drop-piece game-state (scanner) 5 columns) -2)(continue)  )
