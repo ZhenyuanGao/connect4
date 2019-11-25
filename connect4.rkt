d27a29fb9d15c5e40c7b0f2b63b5f37a714d837d
Zhenyuan Gao <kegao@westmont.edu>
2019-11-24 17:08:07 -0800

    kevin's commit

--- connect4.rkt
+++ connect4.rkt
@@ -142,25 +142,65 @@
 ;)
 
 ; game loop
+
+;make a scanner function to get the user input. In this case, it transform a string to a number.
 (define (scanner) (display "Please insert your input")(string->number (read-line (current-input-port) 'any)))
 
+;print the board for the first time
+(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+               (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+                           
+;while winning conditions are not satisfied, we keep doing the game loop
+(define (game-loop)
+  (if (check-win game-state 0 columns)
+      0
+      (let ()
+        (while  (= (drop-piece game-state (scanner) 5 columns) -2)(continue)  )
+                        
 
+        ;(send game-state get-board))
+        ;change turn after each player played and get the newest game state.
+
+        (check-win game-state 0 columns)
+        (display (check-win game-state 0 columns))
+        (send game-state change-turn)
+        ;check the win condition again
+  
+        ;print out the board again.
+        (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+        (game-loop)))) 
  
-(define (game-loop) (while (not (check-win game-state 0 columns))
-                           (add-drawing (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board)))
-
-                           (while  (= (drop-piece game-state (scanner) 5 columns) -2)(add-drawing (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-                                                                                                                 (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))) (continue)  )
-                           (send game-state change-turn)
-
-                           (check-win game-state 0 columns)
-                           (add-drawing (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
-                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board)))
  
-                           )
-  )
+  ;(display "you win lol")
+  
 
-(send f show #t)
-(game-loop)
+
+;(define (game-loop) (cond [(not (check-win game-state 0 columns))(let () ()     )]))
+
+;show the board object
+;(send f show #t)
+;
+;(drop-piece game-state (scanner) 5 columns)
+;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+;(drop-piece game-state (scanner) 5 columns)
+;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+;(drop-piece game-state (scanner) 5 columns)
+;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+;(drop-piece game-state (scanner) 5 columns)
+;(maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+;                                                       (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+
+
+;(check-win game-state 0 columns)
+;call game loop
+ (game-loop )
 ;()
+;(while  (= (drop-piece game-state (scanner) 5 columns) -2)(continue)  )
+;                         (maker-helper  (ch  (for/list ([i (in-range 20 150 20)]) i) 6)
+;                                                    (sort (ch  (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board))
+;while user did not give a valid input, keep asking the user to input again.
+;(while  (= (drop-piece game-state (scanner) 5 columns) -2)(continue)  )
