3d304c178f5bd7342009dfded8a0c31d8504e018
greeeen89 <greeeenmango89@gmail.com>
2019-11-25 03:32:15 -0800

    -add calculate utility procedure which returns a number value for a give state

--- connect4ai.rkt
+++ connect4ai.rkt
@@ -2,6 +2,7 @@
 ;(require "connect4.rkt")
 (require racket/vector)
 (require "game-functions.rkt")
+;(require "Connect_four_window.rkt")
 (provide (all-defined-out))
 
 (define greeting "Hello human.")
@@ -13,12 +14,45 @@
 
 ; 0 0 0 2 0 0 0 2 1 2 1 2 1 2 2 1 2 1 2 1 2 1 2 1 1 2 2 1 1 1 1 2 1 2 1 2 2 1 1 1 2 1
 
-(define game-state (new state% [current-board (vector 0 0 0 2 0 0 0
-                                                      2 1 2 1 2 1 2
-                                                      2 1 2 1 2 1 2
-                                                      1 2 1 1 2 2 1
-                                                      1 1 1 2 1 2 1
-                                                      2 2 1 1 1 2 1)] [current-turn 2] [current-status 1]))
+(define game-state (new state% [current-board (vector 0 0 0 0 0 0 0
+                                                      0 0 0 0 0 0 0
+                                                      0 0 0 0 0 0 0
+                                                      0 0 0 0 1 0 0
+                                                      0 0 2 1 1 0 0
+                                                      0 0 1 2 1 0 0)] [current-turn 2] [current-status 1]))
+
+(define (calculate-dir-value board turn prev-position columns get-next-pos)
+  (define position (get-next-pos prev-position columns))
+  (cond
+    ; return with no added value if continuing means going off of the board
+    [(or (and (equal? (modulo prev-position columns) 0) ; check if previous piece is at left border
+              (equal? (modulo position columns) (- columns 1))) ; check if the next piece in the direction would pass border
+         (and (equal? (modulo prev-position columns) (- columns 1)) ; check if piece is at right border
+              (equal? (modulo position columns) 0)) ; check if the next piece in the direction would pass border
+         (> position (- (vector-length board) 1))) 0] ; pass bottom border
+    ; if piece is the correct color, continue add value and continue in same direction
+    [(equal? (vector-ref board position) turn) (+ (check-dir board turn position columns get-next-pos) 1)]
+    ; if piece is a blank space after a correct color piece, return with added value
+    [(and (equal? (vector-ref board prev-position) turn) (equal? (vector-ref board position) 0)) 1]
+    ; no added value
+    [else 0]))
+
+(define (total-dir-value board turn position columns)
+  (apply + (cons (calculate-dir-value board turn position columns (lambda (p n) (+ p (- n 1)))) ; down and left
+                 (cons (calculate-dir-value board turn position columns (lambda (p n) (+ p n))) ; down
+                       (cons (calculate-dir-value board turn position columns (lambda (p n) (+ p (+ n 1)))) ; down and right
+                             (cons (calculate-dir-value board turn position columns (lambda (p n) (+ p 1))) '())))))) ; right
+
+; helper function for calculate-utility
+(define (calculate-utility-helper board turn position columns)
+  (cond
+    [(> position (- (vector-length board) 1)) 0] ; return 0 when the end has been reached
+    [(or (equal? (vector-ref board position) turn) (equal? (vector-ref board position) 0)) (+ (total-dir-value board turn position columns) (calculate-utility-helper board turn (+ position 1) columns))] ; if selected position matches player's piece or is empty, check pieces around it for value adding features                                                  
+    [else (calculate-utility-helper board turn (+ position 1) columns)])) ; selected position has opponent's piece, so go to next position
+
+; returns the value of a given state as a number
+(define (calculate-utility state columns)
+  (calculate-utility-helper (send state get-board) (send state get-turn) 0 columns))
 
 (define (get-result-state state action columns)
   (define result-state (new state%
@@ -79,21 +113,27 @@
         [else 0]) ; 0 value tie
       (min default-value (get-max-values state (get-actions state columns) columns))))
 
-(define (max-value-pos value-list)
-  (define (max-value-pos-helper current-max value-list max-depth depth)
-    (if (eq? value-list null)
-        max-depth ; return when list is empty
-        (if (> current-max (car value-list))
-            (max-value-pos-helper current-max (cdr value-list) depth depth)
-            (max-value-pos-helper (car value-list) (cdr value-list) max-depth depth))))
+(define (max-value-position values actions)
+  (define (max-value-pos-helper max-value values max-action actions)
+    ;(print "max val")
+    ;(print max-value)
+    ;(print "max action")
+    ;(print max-action)
+    (if (eq? values null)
+        max-action ; return when list is empty
+        (if (> max-value (car values))
+            (max-value-pos-helper max-value (cdr values) max-action (cdr actions))
+            (max-value-pos-helper (car values) (cdr values) (car actions) (cdr actions)))))
 
-  (max-value-pos-helper -100 value-list 0 0))
+  (max-value-pos-helper -100 values 0 actions))
 
 
 ; returns the best action or actions if tied
 (define (get-best-action state columns)
-  (print (get-actions state columns))
-  (max-value-pos(get-min-values state (get-actions state columns) columns)))
+  (define actions (get-actions state columns))
+  ;(print actions)
+  (print (get-min-values state actions columns))
+  (max-value-position (get-min-values state actions columns) actions))
 
 
 (define (get-input state column rows columns)
@@ -103,14 +143,16 @@
         (get-input state (string->number (read-line)) rows columns))))
 
 (define (gameloop state)
-  (print (send game-state get-board))
+  ;(print (maker-helper (ch (for/list ([i (in-range 20 150 20)]) i) 6)
+    ;                                                   (sort (ch (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board)))
   (get-input state (string->number (read-line)) 5 7)
-  (when (check-win state 0 7) (print "Human wins!"))
+  (when (equal? (check-win state 0 7) 1) (print "Human wins!"))
   (send state change-turn)
 
-  (print (send game-state get-board))
+  ;(print (maker-helper (ch (for/list ([i (in-range 20 150 20)]) i) 6)
+   ;                                                    (sort (ch (for/list ([i (in-range 20 130 20)]) i) 7) <) (send game-state get-board)))
   (print(drop-piece state (get-best-action state 7) 5 7))
-  (when (check-win state 0 7) (print "Computer wins!"))
+  (when (equal? (check-win state 0 7) 2) (print "Computer wins!"))
   (send state change-turn)
 
   (gameloop state))
